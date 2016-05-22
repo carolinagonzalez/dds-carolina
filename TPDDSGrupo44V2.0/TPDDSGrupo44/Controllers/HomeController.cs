b550792f38a5a143ca49b24dd449761db43fded3
pyritewolf <pyritewolf@protorol.com>
2016-05-22 17:50:50 -0300

    Actualización Búsqueda de Cercanía
    + Modificada la búsqueda de cercanía en Home Controller para que se
    adapte al nuevo modelo de ParadaDeColectivo

--- TPDDSGrupo44V2.0/TPDDSGrupo44/Controllers/HomeController.cs
+++ TPDDSGrupo44V2.0/TPDDSGrupo44/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
+using System.Collections.Generic;
 
 namespace TPDDSGrupo44.Controllers
 {
@@ -17,24 +18,46 @@ namespace TPDDSGrupo44.Controllers
         [HttpPost]
         public ActionResult Index(FormCollection collection)
         {
+
+            //DATOS HARDCODEADOS PARA SIMUAR DB
+            List<Models.ParadaDeColectivo> paradas = new List<Models.ParadaDeColectivo>();
+
+            // Agrego parada 114 - cercana
+            Models.ParadaDeColectivo parada = new Models.ParadaDeColectivo("Mozart 2389", new Models.Coordenada(-34.659690, -58.468764), new Models.ConsultoCercania());
+            parada.palabraClave = "114";
+            paradas.Add(parada);
+
+            // Agrego Parada 36 - lejana
+            parada = new Models.ParadaDeColectivo("Av Escalada 2680", new Models.Coordenada(-34.662325, -58.473300), new Models.ConsultoCercania());
+            parada.palabraClave = "36";
+            paradas.Add(parada);
+
+            //Defino ubicación actual
+            Models.Coordenada dispositivoTactil = new Models.Coordenada(-34.6597047, -58.4688947);
+
+            //FIN DE SIMULACION DE DATOS DE DB
+
+
             try
             {
-                Models.ParadaDeColectivo punto = new Models.ParadaDeColectivo();
 
-                punto.palabraClave = collection["palabraClave"];
-
-                if ( punto.valida(punto.palabraClave) )
+                foreach (Models.ParadaDeColectivo punto in paradas) // Loop through List with foreach.
                 {
-                    ViewBag.SearchText = "¡Punto cercano!";
-                    ViewBag.Search = "ok";
-                    return View();
+                    if (punto.estaCerca(dispositivoTactil) && punto.palabraClave == collection["palabraClave"] )
+                    {
+                        ViewBag.SearchText = "¡Punto cercano!";
+                        ViewBag.Search = "ok";
+                        break;
+                    }
+                    else
+                    {
+                        ViewBag.SearchText = "Punto lejano o desconocido.";
+                        ViewBag.Search = "error";
+                    }
                 }
-                else
-                {
-                    ViewBag.SearchText = "Punto lejano o desconocido.";
-                    ViewBag.Search = "error";
-                    return View();
-                }
+                
+                return View();
+                
             }
             catch
             {
