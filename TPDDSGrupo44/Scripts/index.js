1e2e962726bad03e2a06c2644a26d8c6d95e837d
pyritewolf <jmlanghe@gmail.com>
2016-05-23 22:34:01 -0300

    Update de packages
    + Actualizados todos los packages
    + Agregados cambios mínimos de estilo

---
+++ TPDDSGrupo44/Scripts/index.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    $("input[name='palabraClave']").on("keyup", function () {
+        if ($(this).val()) {
+            $("button").addClass("call-to-action");
+        } else {
+            $("button").removeClass("call-to-action");
+        }
+    });
+
+})
