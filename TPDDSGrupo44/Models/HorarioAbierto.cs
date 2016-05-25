0ae2385b2c75ca7101662a8f97ee45aa63f1944d
carolinagonzalez <carolinagonzalez.utn@gmail.com>
2016-05-25 15:56:53 -0300

    Creo constructor HorarioAbierto
    - Creo constructor
    - Modifico nombre de la clase
    - Modifico tipo de atributo dia

---
+++ TPDDSGrupo44/Models/HorarioAbierto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TPDDSGrupo44.Models
+{
+    public class HorarioAbierto
+    {
+      
+        public DayOfWeek dia { get; set; }
+        public int horarioInicio { get; set; }
+        public int horarioFin { get; set; }
+
+
+        //Creo Constructor del HorarioAbierto
+        public HorarioAbierto(DayOfWeek dia, int horarioInicio, int horarioFin)
+        {
+            this.dia = dia;
+            this.horarioInicio = horarioInicio;
+            this.horarioFin = horarioFin;
+        }
+
+    }
+}
