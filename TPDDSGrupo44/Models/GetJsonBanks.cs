e5d4cf19206043cc5c05a6d7f7be7272deec1ded
Damian <f.93522@gmail.com>
2016-06-28 00:54:06 -0300

    another update

--- TPDDSGrupo44/Models/GetJsonBanks.cs
+++ TPDDSGrupo44/Models/GetJsonBanks.cs
@@ -1,46 +1,46 @@
 ﻿using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Net;
+using System.Globalization;
+using System.Web.Mvc;
 
 namespace TPDDSGrupo44.Models
 {
-    public class GetJsonBanks
+    public class GetJsonBanks : Controller
     {
-        public class Bank
+        private static List<JsonBank> getJsonData(string bank, string service)
         {
-            public string banco { get; set; }
-            public double x { get; set; }
-            public double y { get; set; }
-            public string sucursal { get; set; }
-            public string gerente { get; set; }
-            public List<string> servicios { get; set; }
-        }
+            string url = "http://trimatek.org/Consultas/banco?banco=" + bank + "&servicio=" + service;
+            var jsonString = string.Empty;
 
+            var client = new WebClient();  //hay 2 formas de hacer esto: esta, y otra con la clase HttpWebRequest que permite configurar mas cosas
+            jsonString = client.DownloadString(url);
 
-        public static List<Bank> requestBanks(string banco, string servicio)
-        {
-            string jsonData = getJsonData(banco, servicio);
-
-            List<Bank> banks = JsonConvert.DeserializeObject<List<Bank>>(jsonData);
+            List<JsonBank> banks = JsonConvert.DeserializeObject<List<JsonBank>>(jsonString);
             return banks;
         }
 
+        // no se como se llamaría a esta función, y como convertir las coordenadas dadas en un calle
 
-        private static string getJsonData(string banco, string servicio)
+        public static void showBanks(string bank, string service)
         {
-            string url = "http://trimatek.org/Consultas/banco?banco=" + banco + "&servicio=" + servicio;
-            var jsonString = string.Empty;
-            var client = new WebClient();
+            List<JsonBank> jsonData = getJsonData(bank, service);
 
-            jsonString = client.DownloadString(url);
+            if (jsonData.Count > 0)
+            {
+                foreach(JsonBank something in jsonData) // esto lo copie del home controller digamos
+                {
+                    ViewBag.SearchText = something.Nombre + " -> " + address; // address seria la direccion del lugar, que se genera con las coord.
+                    ViewBag.Search = "Ok";
+                }
+            }
+            else
+            {
+                ViewBag.SearchText = "No se ha encontrado el banco/servicio especificado.";  //me marca los ViewBag en rojo, no se porque
+                ViewBag.Search = "Error";
+            }
 
-            return jsonString;
         }
 
     }
 }
-
-
-
-
-
---
+++ TPDDSGrupo44/Models/JsonBank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPDDSGrupo44.Models
+{
+    public class JsonBank
+    {
+        public class Bank
+        {
+            public string Banco { get; set; }
+            public double X { get; set; }
+            public double Y { get; set; }
+            public string Sucursal { get; set; }
+            public string Gerente { get; set; }
+            public List<string> Servicios { get; set; }
+        }
+    }
+}
