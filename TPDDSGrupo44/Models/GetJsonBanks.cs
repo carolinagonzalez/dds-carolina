16666829daa0ce279d228498e25759ae181bf2f2
Damian <f.93522@gmail.com>
2016-06-27 23:40:24 -0300

    subo clase json

---
+++ TPDDSGrupo44/Models/GetJsonBanks.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace TPDDSGrupo44.Models
+{
+    public class GetJsonBanks
+    {
+        public class Bank
+        {
+            public string banco { get; set; }
+            public double x { get; set; }
+            public double y { get; set; }
+            public string sucursal { get; set; }
+            public string gerente { get; set; }
+            public List<string> servicios { get; set; }
+        }
+
+
+        public List<Bank> requestBanks(string banco, string servicio)
+        {
+            string jsonData = getJsonData(banco, servicio);
+
+            List<Bank> banks = JsonConvert.DeserializeObject<List<Bank>>(jsonData);
+            return banks;
+        }
+
+
+        private string getJsonData(string banco, string servicio)
+        {
+            string url = "http://trimatek.org/Consultas/banco?banco=" + banco + "&servicio=" + servicio;
+            var jsonString = string.Empty;
+            var client = new WebClient();
+
+            jsonString = client.DownloadString(url);
+
+            return jsonString;
+        }
+
+    }
+}
+
+
+
+
+
