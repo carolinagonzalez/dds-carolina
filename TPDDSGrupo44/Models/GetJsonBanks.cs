ecb08197f7f0e4a311f0c46125937906ddeb09a1
Ivan Rodriguez <ivanrodriguezr@outlook.com>
2016-07-01 03:10:02 -0300

    comento lo de getJsonBanks

--- TPDDSGrupo44/Models/GetJsonBanks.cs
+++ TPDDSGrupo44/Models/GetJsonBanks.cs
@@ -30,15 +30,15 @@ namespace TPDDSGrupo44.Models
             {
                 foreach(JsonBank something in jsonData) // esto lo copie del home controller digamos
                 {
-                    ViewBag.SearchText = something.Nombre + " -> " + address; // address seria la direccion del lugar, que se genera con las coord.
+                   // ViewBag.SearchText = something.Nombre + " -> " + address; // address seria la direccion del lugar, que se genera con las coord.
                     //ViewBag.SearchText = something.banco + " -> " + address;
-                    ViewBag.Search = "Ok";
+                   // ViewBag.Search = "Ok";
                 }
             }
             else
             {
-                ViewBag.SearchText = "No se ha encontrado el banco/servicio especificado.";  //me marca los ViewBag en rojo, no se porque
-                ViewBag.Search = "Error";
+                //ViewBag.SearchText = "No se ha encontrado el banco/servicio especificado.";  //me marca los ViewBag en rojo, no se porque
+                //ViewBag.Search = "Error";
             }
 
         }
