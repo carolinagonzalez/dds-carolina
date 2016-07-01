9ea67e942c3b7e5838d530db1660281f1588234e
Ivan Rodriguez <ivanrodriguezr@outlook.com>
2016-07-01 03:46:00 -0300

    Tests cargados!

---
+++ TPDDSGrupo44Tests/Models/ParadaDeColectivoTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Data.Entity.Spatial;
+using System.Device.Location;
+
+namespace TPDDSGrupo44.Models.Tests
+{
+    [TestFixture()]
+    public class ParadaDeColectivoTests
+    {
+        [Test()]
+        public void estaDisponibleTest()
+        {
+            // Agrego parada 114
+            Models.ParadaDeColectivo parada = new Models.ParadaDeColectivo("Mozart 2389", DbGeography.FromText("-34.659690 -58.468764)"));
+
+            parada.palabraClave = "114";
+
+            Assert.IsTrue(parada.estaDisponible(DateTime.Now));
+
+
+
+        }
+    }
+}
