beb53f4f7e0516725f1133098112dd9d565bee4c
carolinagonzalez <carolinagonzalez.utn@gmail.com>
2016-08-23 22:23:41 -0300

    Entrega 3 y 4 - Envio de mails
    - Entrega 3:
    Notificar al Administrador por mail en caso que una búsqueda demore más
    de X segundos. Este valor debe ser parametrizable.
    - Entrega 4:
    Se envía un mail al Usuario avisando el fallo.

--- TPDDSGrupo44/Models/EnvioDeMails.cs
+++ TPDDSGrupo44/Models/EnvioDeMails.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
 using System.Net.Mail;
 
 namespace TPDDSGrupo44.Models
@@ -14,35 +15,39 @@ namespace TPDDSGrupo44.Models
         
         public void enviarMail(TimeSpan cantSegDemora, int tipoDeMail)
         {
-            
-            SmtpClient server = new SmtpClient();
-            server.Port = 587;
-            server.Host = "smtp.gmail.com";
-            server.EnableSsl = true;
-            server.UseDefaultCredentials = false;
-            server.Credentials = new System.Net.NetworkCredential("dds44utnviernes@gmail.com", "carodds44");
-
-            MailMessage mnsj2 = new MailMessage();
-            mnsj2.From  = new MailAddress("dds44utnviernes@gmail.com");
-            mnsj2.Sender = new MailAddress("dds44utnviernes@gmail.com");
-            switch (tipoDeMail)
+            try
             {
-                case 0:
-                    mnsj2.Subject = "Notificacion de busqueda";
-                    mnsj2.Body = "Se Notifica al Administrador en caso que una búsqueda demore más de X segundos";
-                    ; break;
-                case 1:
-                    mnsj2.Subject = "Fallo la aplicacion";
-                    mnsj2.Body = "Se Notifica al Administrador que hubo un error. Revisar LOGS";
-                    ; break;
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("dds44utnviernes@gmail.com");
+                mail.To.Add("dds44utnviernes@gmail.com");
+
+                switch (tipoDeMail)
+                {
+                    case 0:
+                        mail.Subject = "Notificacion de busqueda";
+                        mail.Body = "Se Notifica al Administrador en caso que una búsqueda demore más de X segundos";
+                        break;
+                    case 1:
+                        mail.Subject = "Fallo la aplicacion";
+                        mail.Body = "Se Notifica al Administrador que hubo un error. Revisar LOGS";
+                        break;
+                }
+                
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("dds44utnviernes@gmail.com", "carodds44");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Console.WriteLine("mail Send");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
             }
 
-            mnsj2.To.Add(new MailAddress("dds44utnviernes@gmail.com"));
 
-            /* Enviar */
-            server.Send(mnsj2);
-            server.SendAsync(mnsj2, "Pruebita");
-            server.Dispose();
         }
 
     }
