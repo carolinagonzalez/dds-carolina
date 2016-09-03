7a1b084ffd462b623cc01b6aee2bf700a35ae000
carolinagonzalez <carolinagonzalez.utn@gmail.com>
2016-09-03 01:24:08 -0300

    E4 - Proceso 3 Se pasa a controlador

---
+++ TPDDSGrupo44/Models/Scripts/index.js
@@ -0,0 +1,35 @@
+﻿
+function initMap(place, text) {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+        center: place,
+        zoom: 18
+    });
+
+    var marker = new google.maps.Marker({
+        position: place,
+        title: text
+    });
+
+    marker.setMap(map);
+}
+
+$(document).ready(function () {
+
+    $("input[name='palabraClave']").on("keyup", function () {
+        if ($(this).val()) {
+            $("button").addClass("call-to-action");
+        } else {
+            $("button").removeClass("call-to-action");
+        }
+    });
+
+    $(".poi").on("click", function () {
+        $(".poi").not($(this)).children(".detalles").slideUp(500);
+        $(".poi").not($(this)).removeClass("selected");
+        $(this).children(".detalles").slideToggle(500);
+        $(this).toggleClass("selected")
+    });
+
+
+});
