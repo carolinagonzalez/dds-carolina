c77ff0666d9af3bc1c9994104758d219558e889e
carolinagonzalez <carolinagonzalez.utn@gmail.com>
2016-09-17 17:57:50 -0300

    E5.3.1 Login
    Agrego acción Registrar

---
+++ TPDDSGrupo44/Controllers/CuentaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPDDSGrupo44.Models;
+//using MvcLoginRegistration.Models;
+
+namespace TPDDSGrupo44.Controllers
+{
+    public class CuentaController : Controller
+    {
+        // GET: Cuenta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public Action Registrar(CuentaDeUsuario cuenta)
+        {
+            if (ModelState.IsValid)
+            {
+                using (POIDbContext db = new POIDbContext())
+                {
+                    db.cuentaDeUsuario.Add(cuenta);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = cuenta.Nombre + " " + cuenta.Apellido + "Usuario Registrado Correctamente.";
+            }
+            return ViewBag.Message;
+        }             
+    }
+}
