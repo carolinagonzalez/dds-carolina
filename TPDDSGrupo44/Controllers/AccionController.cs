b05d274f8c5c837fba127c5f844970706d78cb76
carolinagonzalez <carolinagonzalez.utn@gmail.com>
2016-09-22 19:40:13 -0300

    E5-3-1 Pantalla Configuración De Acciones
    Agrego controlador para la acción "Configurar"

---
+++ TPDDSGrupo44/Controllers/AccionController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TPDDSGrupo44.Models;
+
+namespace TPDDSGrupo44.Controllers
+{
+    public class AccionController : Controller
+    {
+        // GET: Accion
+        public ActionResult Index()
+        {
+            using (POIDbContext db = new POIDbContext())
+            {
+                return View(db.configuracionDeAcciones.ToList());
+
+            }
+        }
+
+
+        public ActionResult Configurar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Configurar(ConfiguracionDeAcciones accion)
+        {
+            if (ModelState.IsValid)
+            {
+                using (POIDbContext db = new POIDbContext())
+                {
+                    db.configuracionDeAcciones.Add(accion);
+                    db.SaveChanges();
+                }
+
+                ModelState.Clear();
+
+                ViewBag.Message = accion.nombreAccion + "Fue configurada correctamente";
+            }
+            return View();
+        }
+
+    }
+}
