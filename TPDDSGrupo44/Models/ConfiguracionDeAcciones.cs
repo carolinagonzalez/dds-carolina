6b3425ee61e2c5fb98e53aedec648875bf29b44e
carolinagonzalez <carolinagonzalez.utn@gmail.com>
2016-09-22 19:38:12 -0300

    E5-3-1 Pantalla Configuración De Acciones
    Agrego clase ConfiguracionDeAcciones la cual contiene los atributos
    junto a su respectiva clave primaria

---
+++ TPDDSGrupo44/Models/ConfiguracionDeAcciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TPDDSGrupo44.Models
+{
+    public class ConfiguracionDeAcciones
+    {
+        [Key]
+        public int configID { get; set; }
+
+        public string nombreAccion { get; set; }
+
+        public List<string> config { get; set; }
+    }
+}
