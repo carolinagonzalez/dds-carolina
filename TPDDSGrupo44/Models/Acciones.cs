987a6c3c38eea30fb58cc5f915df82eca818c97c
carolinagonzalez <carolinagonzalez.utn@gmail.com>
2016-09-22 19:36:31 -0300

    E5-3-1 Pantalla Configuración De Acciones
    Agrego la clase Acciones la cual contiene "posibles" acciones a ser
    agregadas.

---
+++ TPDDSGrupo44/Models/Acciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPDDSGrupo44.Models
+{
+    public class Acciones
+    {
+        public int AccionID { get; set; }
+        public string AccionNombre { get; set; }
+
+        public static IQueryable<Acciones> GetAcciones()
+        {
+            return new List<Acciones>
+            {
+                new Acciones
+                {
+                    AccionID = 1,
+                    AccionNombre = "Generar Log"
+                 },
+                new Acciones{
+                    AccionID = 2,
+                    AccionNombre = "Bajar POI"
+                }
+            }.AsQueryable();
+        }
+    }
+}
