4f111ab5a3391e4cb3d79bfc6daadd8d616f7116
carolinagonzalez <carolinagonzalez.utn@gmail.com>
2016-09-30 16:31:45 -0300

    E5 - Controlador de Historial de Busqueda

---
+++ TPDDSGrupo44/Controllers/HistorialDeBusquedaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TPDDSGrupo44.Controllers
+{
+    public class HistorialDeBusquedaController : Controller
+    {
+        // GET: HistorialDeBusqueda
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
